0e392b108e9bf0a154938f42d7d44f8b171a8f28
bjorksander-netizen <bjorksander@gmail.com>
2026-07-19 18:59:49 +0700

    fix: add missing MainActivity import in HomeFragment

--- android-app/app/src/main/java/com/autopad/ui/HomeFragment.kt
+++ android-app/app/src/main/java/com/autopad/ui/HomeFragment.kt
@@ -14,6 +14,7 @@ import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.autopad.R
+import com.autopad.MainActivity
 import com.autopad.connection.ConnectionManager
 
 class HomeFragment : Fragment() {
